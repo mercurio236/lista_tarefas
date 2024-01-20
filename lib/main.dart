eefa2ea9544f86697c9bba8d271bee19ed23e9d1
Arley Souto <macx15@hotmail.com>
2024-01-20 19:28:35 -0300

    feat: criado logiga de adicionar na lista

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,19 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   //aqui as logicas
+  final addFieldController = TextEditingController();
   List _todoList = [];
+
+  void _addTodo() {
+    setState(() {
+      Map<String, dynamic> newTodo =
+          Map(); //quando usar json sempre vamos usar o dynamic
+      newTodo['title'] = addFieldController.text;
+      addFieldController.text = '';
+      newTodo['ok'] = false;
+      _todoList.add(newTodo);
+    });
+  }
 
   Future<File> _getFile() async {
     final directory = await getApplicationCacheDirectory();
@@ -44,6 +56,67 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Lista de tarefas',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.fromLTRB(17, 1, 7, 1),
+            child: Row(children: [
+              //aqui vai calcular o tamanho da tela corretamente
+              Expanded(
+                child: TextField(
+                  controller: addFieldController,
+                  decoration: InputDecoration(
+                    labelText: 'Nova tarefa',
+                    labelStyle: TextStyle(color: Colors.blueAccent),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: _addTodo,
+                child: Text(
+                  'ADD',
+                  style: TextStyle(color: Colors.white),
+                ),
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll(Colors.blueAccent),
+                    shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(3)))),
+              )
+            ]),
+          ),
+          Expanded(
+            child: ListView.builder(
+                //são listas que não vai ser renderizado se não estiver sendo exibido
+                padding: EdgeInsets.only(top: 10),
+                itemCount: _todoList.length,
+                itemBuilder: (context, index) {
+                  return CheckboxListTile(
+                    title: Text(_todoList[index]['title']),
+                    value: _todoList[index]['ok'],
+                    secondary: CircleAvatar(
+                      child: Icon(
+                          _todoList[index]['ok'] ? Icons.check : Icons.error),
+                    ),
+                    onChanged: (check){
+                      setState(() {
+                        _todoList[index]["ok"] = check;
+                      });
+                    },
+                  );
+                }),
+          ),
+        ],
+      ),
+    );
   }
 }
